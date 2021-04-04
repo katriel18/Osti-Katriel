ae76bde750d07fb94a3ba9b91857ddd5523163d1
Osti Katriel <katriel181920@example.com>
2021-04-04 18:37:03 -0500

    two themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-web-site';
+
+  cssUrl: string;
+
+  constructor(public sanitizer: DomSanitizer) {
+    this.cssUrl = 'assets/styles/saga-green/theme.css';
+  }
+
+  changeStyle() {
+    //this.cssUrl = (this.cssUrl === `node_modules/primeng/resources/themes/vela-green/theme.css`) ? `node_modules/primeng/resources/themes/saga-green/theme.css` : `node_modules/primeng/resources/themes/vela-green/theme.css`;
+  
+
+    this.cssUrl = (this.cssUrl === 'assets/styles/saga-green/theme.css') ? 'assets/styles/vela-green/theme.css' : 'assets/styles/saga-green/theme.css';
+    //this.cssUrl = 'assets/styles/vela-green/theme.css';
+  
+  }
+
 }
