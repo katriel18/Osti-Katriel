03ca3afb9a02bdd2d4e63c54fcd3361f9fa5cdf0
katriel <katriel181920@gmail.com>
2021-04-06 21:04:02 -0500

    scrolltop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,18 @@ export class AppComponent {
   title = 'my-web-site';
 
   cssUrl: string;
-  //dark:boolean;
   mode:string;
  
   constructor(public sanitizer: DomSanitizer) {
     this.cssUrl = 'assets/styles/saga-green/theme.css';
-    //this.dark=false;
     this.mode='pi pi-moon';
 
   }
 
   changeStyle() {
-    //this.cssUrl = (this.cssUrl === `node_modules/primeng/resources/themes/vela-green/theme.css`) ? `node_modules/primeng/resources/themes/saga-green/theme.css` : `node_modules/primeng/resources/themes/vela-green/theme.css`;
   
-
     this.cssUrl = (this.cssUrl === 'assets/styles/saga-green/theme.css') ? 'assets/styles/vela-green/theme.css' : 'assets/styles/saga-green/theme.css';
-    //this.dark = (this.dark === false) ? true : false;
     this.mode = (this.mode === 'pi pi-moon') ? 'pi pi-sun' : 'pi pi-moon';
-    //this.cssUrl = 'assets/styles/vela-green/theme.css';
   
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 
 import {ButtonModule} from 'primeng/button';
 import {GalleriaModule} from 'primeng/galleria';
+import {ScrollTopModule} from 'primeng/scrolltop';
+import {ScrollPanelModule} from 'primeng/scrollpanel';
 
+import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 @NgModule({
   declarations: [
@@ -18,11 +22,15 @@ import { HomeComponent } from './pages/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,//scroll top
+    
 
     HttpClientModule,
 
     ButtonModule,
-    GalleriaModule
+    GalleriaModule,
+    ScrollTopModule,
+    ScrollPanelModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
